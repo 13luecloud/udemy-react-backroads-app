1b9fd534127b70e2af195e6d5cd707fca510bedd
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-06-17 23:30:40 +0800

    Updated Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <section className="hero" id="home">
+            <div className="hero-banner">
+                <h1> Continue exploring</h1>
+                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, corrupti et neque eveniet sit nam voluptatibus optio veniam qui magnam facilis at dolorem consequuntur totam blanditiis autem in rerum tenetur?</p>
+                <a href="#tours" className="btn hero-btn">
+                    Explore tours
+                </a>
+            </div> 
+
+        </section>
+    )
+}
+
+export default Hero
