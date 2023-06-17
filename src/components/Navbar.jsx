e88c5db15843b50ee4b58a77c38dc009f7e3847d
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-06-18 06:57:02 +0800

    Added social media links data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logoImg from "../images/logo.svg";
+import { socialMedias } from "../data.jsx"
 
 const Navbar = () => {
     return (
@@ -12,39 +13,48 @@ const Navbar = () => {
                 </div>
 
                 <ul class="nav-links" id="nav-links">
-                <li>
-                    <a href="#home" class="nav-link"> home </a>
-                </li>
+                    <li>
+                        <a href="#home" class="nav-link"> home </a>
+                    </li>
 
-                <li>
-                    <a href="#about" class="nav-link"> about </a>
-                </li>
+                    <li>
+                        <a href="#about" class="nav-link"> about </a>
+                    </li>
 
-                <li>
-                    <a href="#services" class="nav-link"> services </a>
-                </li>
+                    <li>
+                        <a href="#services" class="nav-link"> services </a>
+                    </li>
 
-                <li>
-                    <a href="#tours" class="nav-link"> tours</a>
-                </li>
+                    <li>
+                        <a href="#tours" class="nav-link"> tours</a>
+                    </li>
                 </ul>
 
                 <ul class="nav-icons">
-                <li>
-                    <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-                        <i class="fab fa-facebook" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-                        <i class="fab fa-twitter" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.twitter.com" target="_blank" class="nav-icon">
-                        <i class="fab fa-squarespace" />
-                    </a>
-                </li>
+                    {/* {socialMedias.map((socialMedia) => {
+                        return (
+                            <li>
+                                <a href={socialMedia.link} target="_blank" className="nav-icon">
+                                    <i className={"fab fa-".socialMedia.icon_name} />
+                                </a>
+                            </li>
+                        )
+                    })} */}
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+                            <i class="fab fa-facebook" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+                            <i class="fab fa-twitter" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+                            <i class="fab fa-squarespace" />
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
---
+++ src/data.jsx
@@ -0,0 +1,14 @@
+const SocialMedias = [
+    {
+        link: "https://www.facebook.com",
+        icon_name: "facebook"
+    }, 
+    {
+        link: "https://www.twitter.com",
+        icon_name: "twitter"
+    }, 
+    {
+        link: "https://www.tumblr.com",
+        icon_name: "tumblr"
+    }
+]
